17bf14d59f840061979562a65cbf177a08a496df
5K7-BUG <chavhanakash15@gmail.com>
2024-06-23 02:27:50 +0530

    new file of ec2 run docker nginx

--- main.tf
+++ main.tf
@@ -1,15 +1,107 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "web-server" {
-  ami = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "docker"
+    Name: "${var.env_prefix}-vpc"
   }
 }
-output "ec2-intance-ip" {
-  value = aws_instance.web-server.public_ip
 
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: "${var.env_prefix}-subnet-1"
+  }
 }
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-rtb"
+    }
+}
+
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}igw"
+  }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      prefix_list_ids = []
+  }
+  tags = {
+      Name: "${var.env_prefix}-sg"
+  }
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = "ami-08a0d1e16fc3f61ea"
+    instance_type = "t2.micro"
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = "terraform"
+
+    user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:8080 nginx
+                EOF
+      tags = {
+        Name: "${var.env_prefix}-appserver"
+      }
+}
+
+output "aws_app_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+
+
+
+
+
